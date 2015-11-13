e9c99b468423c7e6033929d164940f997e7c6972
Delong Fu <delongfu@gmail.com>
2015-11-13 13:22:08 -0800

    hockeyapp update test

--- SmartAlarm/app/src/main/java/com/microsoft/smartalarm/AlarmListActivity.java
+++ SmartAlarm/app/src/main/java/com/microsoft/smartalarm/AlarmListActivity.java
@@ -1,11 +1,14 @@
 package com.microsoft.smartalarm;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.view.MenuItem;
 
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.FeedbackManager;
+import net.hockeyapp.android.UpdateManager;
 import net.hockeyapp.android.objects.FeedbackUserDataElement;
 
 public class AlarmListActivity extends SingleFragmentActivity
@@ -35,10 +38,23 @@ public class AlarmListActivity extends SingleFragmentActivity
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        final String hockeyAppId = getResources().getString(R.string.hockeyapp_id);
+        UpdateManager.register(this, hockeyAppId);
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         final String hockeyAppId = getResources().getString(R.string.hockeyapp_id);
         CrashManager.register(this, hockeyAppId);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UpdateManager.unregister();
     }
 
     @Override
